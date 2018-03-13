3c28626efafaad37c500f49371d784602caa604d
Jason Devine <jdevine82@gmail.com>
2018-03-13 11:38:01 +1100

    removed orginal

--- index.php
+++ index.php
@@ -16,41 +16,41 @@
     <!-- Compressed CSS -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/foundation/6.4.1/css/foundation.min.css" />
     <link rel="stylesheet" href="custom.css" />
-
+   <link href="https://fonts.googleapis.com/css?family=Source+Serif+Pro" rel="stylesheet"> 
 
 </head>
 
 <body>
     <div class="grid-container">
-        <div class="grid-y" >
+        <div class="grid-y">
             <div class="cell shrink header">
                 <div class="grid-x auto">
                     <div class="cell  title-bar" data-responsive-toggle="main-menu" data-hide-for="medium">
                         <div class="grid-x auto">
-                        <button class="menu-icon" type="button" data-toggle="main-menu"></button>
-                        <div class="cell small-3 title-bar-title ">Menu</div>
+                            <button class="menu-icon" type="button" data-toggle="main-menu"></button>
+                            <div class="cell small-3 title-bar-title ">Menu</div>
 
 
-                        <div class="cell small-8 top-bar" id="main-menu">
-                            <div class=" top-bar-left">
+                            <div class="cell small-8 top-bar" id="main-menu">
+                                <div class=" top-bar-left">
 
-                                <ul class=" drop-down vertical menu " data-dropdown-menu>
-                                    <li><a href="#">Home</a></li>
-                                    <li><a href="#">About</a></li>
-                                    <li><a href="#">Menu</a></li>
-                                    <li><a href="#">Location</a></li>
-                                    <li><a href="#">Catering</a></li>
-                                    <li><a href="#">Contact Us</a></li>
-                                </ul>
+                                    <ul class=" drop-down vertical menu " data-dropdown-menu>
+                                        <li><a href="#">Home</a></li>
+                                        <li><a href="#">About</a></li>
+                                        <li><a href="#">Menu</a></li>
+                                        <li><a href="#">Location</a></li>
+                                        <li><a href="#">Catering</a></li>
+                                        <li><a href="#">Contact Us</a></li>
+                                    </ul>
+                                </div>
                             </div>
                         </div>
-                </div>
                     </div>
 
                     <div class="cell medium-2">
                         <img src="img/logo.png" alt="Little Pea Bakery logo" width="98" height="76" id="logo">
                     </div>
-                    <div class="cell auto"></div>
+                    <div class="cell auto"><h1>Baker's Galore deserts lollies pastry sweet</h1></div>
                     <div class="cell medium-4">
                         <form action="#" method="post" id="form-search">
                             <div class="input-group">
@@ -78,15 +78,15 @@
 
                     </div>
                     <div class="cell small-auto medium-auto large-auto mainpage ">
-                        <h1>You deserve a sweet little treat every now and then.</h1>
+                        <h2>You deserve a sweet little treat every now and then.</h2>
                         <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. Donec non enim in turpis pulvinar facilisis. Ut felis.</p>
                         <div class="grid-x auto">
                             <div class="cell small-12 medium-6 large-4 flex-container">
                                 <div class="feature card" id="feature-candy">
 
                                     <div class="card-section">
-                                        <h2>Candies</h2>
-                                        <h3>Truffles, Bars, Caramels, and more</h3>
+                                        <h3>Candies</h3>
+                                        <h4>Truffles, Bars, Caramels, and more</h4>
                                         <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. </p>
                                     </div>
                                 </div>
@@ -94,22 +94,68 @@
 
                             <div class="cell small-12 medium-6 large-4 flex-container">
                                 <div class="feature card" id="feature-pastry">
-                                    <h2>Pastries</h2>
-                                    <h3>Croissants, Danishes, Cream Puffs, and more</h3>
+                                    <h3>Pastries</h3>
+                                    <h4>Croissants, Danishes, Cream Puffs, and more</h4>
                                     <p>Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est.</p>
                                 </div>
                             </div>
                             <div class="cell small-12 medium-6 large-4 flex-container">
                                 <div class="feature card" id="feature-dessert">
-                                    <h2>Desserts</h2>
-                                    <h3>Cakes, Pies, Tarts, Cookies, and more</h3>
+                                    <h3>Desserts</h3>
+                                    <h4>Cakes, Pies, Tarts, Cookies, and more</h4>
                                     <p>Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante.</p>
                                 </div>
                             </div>
                         </div>
+                        <div class="grid-x auto" id="contactus">
+                            <form class="grid-x grid-margin-x">
+                            <h3 class="cell small-12 ">contact form</h3>
+                                <div class="grid-container fluid">
+                           
+                                <div class="grid-x grid-margin-x">
+
+                                    <div class="small-9 cell">
+                                        <input type="text" id="name-label" placeholder="John Doe">
+                                    </div>
+                                    <div class="small-3 cell">
+                                        <label for="name-label" class="text-left middle">Name</label>
+                                    </div>
+                                </div>
+                                <div class="grid-x grid-margin-x">
+
+                                    <div class="small-9 cell">
+                                        <input type="email" id="middle-label" placeholder="mail@example.com">
+                                    </div>
+                                    <div class="small-3 cell">
+                                        <label for="middle-label" class="text-left middle">Email</label>
+                                    </div>
+                                </div>
+                                <div class="grid-x grid-margin-x">
+
+                                    <div class="small-9 cell">
+                                        <input type="website" id="website-label" placeholder="www.example.com">
+                                    </div>
+                                    <div class="small-3 cell">
+                                        <label for="website-label" class="text-left middle">Website</label>
+                                    </div>
+                                </div>
+                                <div class="grid-x grid-margin-x">
+
+                                    <div class="small-12 cell">
+                                        <textarea placeholder="Write something to us" rows='4'></textarea>
+                                    </div>
+
+                                </div>
+                                <div class="input-group-button">
+                                    <input type="submit" class="button" value="Submit">
+                                </div>
+                                </div>
+                
+                            </form>
+                        </div>
 
                     </div>
-                    <div class="cell small-12 medium-3 large-3  newsfeed">
+                    <div class="cell small-12 medium-8 medium-offset-3 large-offset-0 large-3  newsfeed">
                         <div id="newsletter">
                             <h3>Email Newsletter</h3>
                             <p>Sign up to get monthly recipes, exclusive offers and more.</p>
@@ -138,19 +184,19 @@
                 </div>
             </div>
             <div class="cell shrink footer">
-                  <div id="about">
-                <h2>About this Page</h2>
-                <p>This page is for a fictional site. It was created by <a href="http://www.zomigi.com">Zoe Mickley Gillenwater</a> as an exercise in the book <a href="http://www.stunningcss3.com"><i>Stunning CSS3: A Project-based Guide to the Latest in CSS</i></a>. It uses CSS3 media queries to change the design at different screen sizes; resize your browser window or load it up in a mobile device to check it out.</p>
-            </div>
-            <div id="credits">
-                <h2>Credits</h2>
-                <dl>
-                    <dt>Web Fonts: </dt>
-                    <dd>Nadia Serif from <a href="http://www.kernest.com">Kernest</a></dd>
-                    <dt>Illustrations: </dt>
-                    <dd>Yummy by <a href="http://www.iconeden.com/icon/yummy-free-icons.html">Icon Eden</a></dd>
-                </dl>
-            </div>
+                <div id="about">
+                    <h2>About this Page</h2>
+                    <p>This page is for a fictional site. It was created by <a href="http://www.zomigi.com">Zoe Mickley Gillenwater</a> as an exercise in the book <a href="http://www.stunningcss3.com"><i>Stunning CSS3: A Project-based Guide to the Latest in CSS</i></a>. It uses CSS3 media queries to change the design at different screen sizes; resize your browser window or load it up in a mobile device to check it out.</p>
+                </div>
+                <div id="credits">
+                    <h2>Credits</h2>
+                    <dl>
+                        <dt>Web Fonts: </dt>
+                        <dd>Nadia Serif from <a href="http://www.kernest.com">Kernest</a></dd>
+                        <dt>Illustrations: </dt>
+                        <dd>Yummy by <a href="http://www.iconeden.com/icon/yummy-free-icons.html">Icon Eden</a></dd>
+                    </dl>
+                </div>
             </div>
         </div>
     </div>
